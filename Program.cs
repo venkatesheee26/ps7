a85da40051d30443eef7fda06602297ec20016aa
venkatesheee26@gmail.com <venkatesheee26@gmail.com>
2023-07-14 15:03:03 +0530

    ps7

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ArrayListExample
+{
+    public static void Main(string[] args)
+    {
+      
+        List<string> fruits = new List<string>();
+
+       
+        fruits.Add("apple");
+        fruits.Add("banana");
+        fruits.Add("cherry");
+        fruits.Add("date");
+        fruits.Add("elderberry");
+
+       
+        Console.WriteLine("Total number of elements: " + fruits.Count);
+
+      
+        bool containsDate = fruits.Contains("date");
+        Console.WriteLine("List contains 'date': " + containsDate);
+
+   
+        fruits.Insert(1, "fig");
+
+ 
+       fruits.Remove("banana");
+
+      
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+}
+
+
